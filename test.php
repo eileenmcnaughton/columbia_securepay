732117262d4cb5b57c614ffd988449a57fdf7957
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-06-22 23:53:55 +1200

    Add output & generated codes & iv

--- test.php
+++ test.php
@@ -11,8 +11,22 @@ $apiKey = 'abc';
 $encryptionKey = 'xyz';
 $siteURL = 'https://dmaster.localhost:32353';
 $cipher = 'aes-256-ctr';
+include_once '/securecreds.php';
 // Using the ssl method creates a string which doesn't json well.
+$fp = fopen('output.txt', 'w');
 $iv = random_bytes(16);
+fwrite($fp, 'First the iv is generated');
+fwrite($fp, "\n");
+fwrite($fp, 'Command - random_bytes(16)');
+fwrite($fp, "\n");
+fwrite($fp, 'Generated iv is :');
+fwrite($fp, "\n");
+$ivFile = fopen('iv.bin', 'w');
+fwrite($fp, $iv);
+fwrite($ivFile, $iv);
+fwrite($fp, "\n");
+fwrite($fp, "\n");
+
 try {
   $params = [
     'order_id' => 5,
@@ -37,7 +51,32 @@ try {
     ],
   ];
 
-  $string = base64_encode(openssl_encrypt(json_encode($params), $cipher, $encryptionKey, 0, $iv));
+  fwrite($fp, 'The parameters as json_encoded');
+  fwrite($fp, "\n");
+  fwrite($fp, 'Command - json_encode($params)');
+  fwrite($fp, "\n");
+  $jsonParams = json_encode($params);
+  fwrite($fp, $jsonParams);
+  fwrite($fp, "\n");
+  fwrite($fp, "\n");
+
+  fwrite($fp, 'The json is encrypted');
+  fwrite($fp, "\n");
+  fwrite($fp, 'Command - openssl_encrypt($jsonParams, $cipher, $encryptionKey, 0, $iv)');
+  fwrite($fp, "\n");
+
+  $encryptedParams = openssl_encrypt($jsonParams, $cipher, $encryptionKey, 0, $iv);
+  fwrite($fp, $encryptedParams);
+  fwrite($fp, "\n");
+  fwrite($fp, "\n");
+  fwrite($fp, 'The encrypted string is base64_encoded (so it will make valid json)');
+  fwrite($fp, "\n");
+  fwrite($fp, 'Command - base64_encode($encryptedParams)');
+  fwrite($fp, "\n");
+  $string = base64_encode($encryptedParams);
+  fwrite($fp, $string);
+  fwrite($fp, "\n");
+  fwrite($fp, "\n");
 
   $client = new Client([
     'base_uri' => $siteURL,
@@ -45,11 +84,25 @@ try {
     'headers' => ['X-Civi-Auth' => 'Bearer ' . $apiKey],
   ]);
 
-  $response = $client->post('civicrm/ajax/api4/Securepay/submit', [
-    'form_params' => [
-      'params' => json_encode(['fields' => $string, 'iv' => base64_encode($iv)]),
-    ],
-  ]);
+  fwrite($fp, 'The iv is base64_encoded (so it will make valid json)');
+  fwrite($fp, "\n");
+  fwrite($fp, 'Command - base64_encode($iv)');
+  fwrite($fp, "\n");
+
+  $encodedIv = base64_encode($iv);
+  fwrite($fp, $encodedIv);
+  fwrite($fp, "\n");
+
+  fwrite($fp, "\n");
+  fwrite($fp, 'Finally the postArray is compiled, as json');
+  fwrite($fp, "\n");
+
+  $postValues = ['form_params' => [
+    'params' => json_encode(['fields' => $string, 'iv' => $encodedIv]),
+  ]];
+  fwrite($fp, print_r($postValues, TRUE));
+  fwrite($fp, "\n");
+  $response = $client->post('civicrm/ajax/api4/Securepay/submit', $postValues);
   $results = json_decode((string) $response->getBody(), TRUE);
 }
 catch (Exception $e) {
