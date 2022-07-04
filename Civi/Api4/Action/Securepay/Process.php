2b5666a4d15a187bbd1955e5a536cf7ac6072e47
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-07-04 23:44:20 +1200

    Fix status field name

--- Civi/Api4/Action/Securepay/Process.php
+++ Civi/Api4/Action/Securepay/Process.php
@@ -104,7 +104,7 @@ class Process extends AbstractAction {
 
     $contributionParams = [
       'total_amount' => $securePay['amount'],
-      'status_id:name' => $this->getMappedStatus($securePay['data']['status']),
+      'contribution_status_id:name' => $this->getMappedStatus($securePay['data']['status']),
       'invoice_id' => $securePay['order_id'],
       'is_test' => $securePay['is_test'],
       'receive_date' => $securePay['receive_date'],
